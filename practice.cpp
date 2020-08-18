539f3a3e3c5e47845e44bbc734104b9d8f248c28
Julian <yenhachi8@gmail.com>
2020-08-18 14:52:41 +0800

    linkListPractice

--- practice.cpp
+++ practice.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
+struct node
+{
+    int data;
+    struct node *next;
+};
+typedef struct node Node;
+
 int main()
 {
+    Node a, b, c;
+    Node *ptr = &a;
+    a.data = 12;
+    a.next = &b;
+    b.data = 5;
+    b.next = &c;
+    c.data = 30;
+    c.next = nullptr;
 
-    cout << "Hello" << endl;
+    while (ptr != NULL)
+    {
+        cout << "address= " << ptr << " data= " << ptr->data << endl;
+
+        ptr = ptr->next;
+    }
 }
